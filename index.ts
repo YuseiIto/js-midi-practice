c1194c57fb05d57ce8b58066406fee539f207b95
Yusei Ito <me@yuseiito.com>
2024-06-29 19:44:56 -0700

    feat: かえるのうたを実装

--- index.ts
+++ index.ts
@@ -5,11 +5,31 @@ import MidiWriter from 'midi-writer-js';
 const track = new MidiWriter.Track();
 
 // Define an instrument (optional):
-track.addEvent(new MidiWriter.ProgramChangeEvent({instrument: 1}));
+track.addEvent(new MidiWriter.ProgramChangeEvent({ instrument: 1 }));
 
-// Add some notes:
-const note = new MidiWriter.NoteEvent({pitch: ['C4', 'D4', 'E4'], duration: '4'});
-track.addEvent(note);
+const notes = ['C4', 'D4', 'E4', 'F4', 'E4', 'D4', 'C4',
+	'E4X', 'F4', 'G4', 'A4', 'G4', 'F4', 'E4',
+	'C4X', 'C4X', 'C4X', 'C4X',
+	'C4SX', 'C4S', 'D4S', 'D4S', 'E4S', 'E4S', 'F4S', 'F4S',
+	'E4', 'D4', 'C4'];
+
+
+for (const p of notes) {
+	let wait = null;
+	if (p.includes('X')) {
+		wait = '4';
+	}
+
+	let duration = '4';
+	if (p.includes('S')) {
+		duration = '8';
+	}
+
+	const pitch = [p.replace(/X|S/g, '')];
+
+	const event = new MidiWriter.NoteEvent({ pitch, duration, wait });
+	track.addEvent(event);
+}
 
 const write = new MidiWriter.Writer(track);
 const buf = write.buildFile();
